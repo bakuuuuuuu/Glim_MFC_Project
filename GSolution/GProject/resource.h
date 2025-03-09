44de85f42454f7240e79dec20feda11fb67b3af3
bakuuuuuuu <dudwls5674@naver.com>
2025-03-09 21:02:41 +0900

    first commit

---
+++ GSolution/GProject/resource.h
@@ -0,0 +1,28 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// GProject.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_GPROJECT_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDC_BTN_RESET                   1000
+#define IDC_BTN_RANDOM                  1001
+#define IDC_BTN_APPLY_RADIUS            1002
+#define IDC_EDIT1                       1003
+#define IDC_EDIT_RADIUS                 1003
+#define IDC_STATIC_CIRCLE1              1004
+#define IDC_STATIC_CIRCLE2              1005
+#define IDC_STATIC_CIRCLE3              1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
