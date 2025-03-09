4ce1f1d7cc85f5946df5a484d15668173ee64e56
bakuuuuuuu <dudwls5674@naver.com>
2025-03-09 21:13:14 +0900

    feat: 새로운 기능 추가

--- GSolution/GProject/GProjectDlg.cpp
+++ GSolution/GProject/GProjectDlg.cpp
@@ -1,5 +1,4 @@
-﻿
-// GProjectDlg.cpp: 구현 파일
+﻿// GProjectDlg.cpp: 구현 파일
 //
 
 #include "pch.h"
@@ -18,19 +17,19 @@
 class CAboutDlg : public CDialogEx
 {
 public:
-	CAboutDlg();
+    CAboutDlg();
 
-// 대화 상자 데이터입니다.
+    // 대화 상자 데이터입니다.
 #ifdef AFX_DESIGN_TIME
-	enum { IDD = IDD_ABOUTBOX };
+    enum { IDD = IDD_ABOUTBOX };
 #endif
 
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
-
-// 구현입니다.
 protected:
-	DECLARE_MESSAGE_MAP()
+    virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+    // 구현입니다.
+protected:
+    DECLARE_MESSAGE_MAP()
 };
 
 CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
@@ -39,7 +38,7 @@ CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
 
 void CAboutDlg::DoDataExchange(CDataExchange* pDX)
 {
-	CDialogEx::DoDataExchange(pDX);
+    CDialogEx::DoDataExchange(pDX);
 }
 
 BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
@@ -51,20 +50,31 @@ END_MESSAGE_MAP()
 
 
 CGProjectDlg::CGProjectDlg(CWnd* pParent /*=nullptr*/)
-	: CDialogEx(IDD_GPROJECT_DIALOG, pParent)
+    : CDialogEx(IDD_GPROJECT_DIALOG, pParent), m_selectedCircle(-1), m_isDragging(false), m_isRunning(false)
+    , m_radius(0)
 {
-	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+    m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
 
 void CGProjectDlg::DoDataExchange(CDataExchange* pDX)
 {
-	CDialogEx::DoDataExchange(pDX);
+    CDialogEx::DoDataExchange(pDX);
+    DDX_Control(pDX, IDC_STATIC_CIRCLE1, m_staticCircle1);
+    DDX_Control(pDX, IDC_STATIC_CIRCLE2, m_staticCircle2);
+    DDX_Control(pDX, IDC_STATIC_CIRCLE3, m_staticCircle3);
+    DDX_Text(pDX, IDC_EDIT_RADIUS, m_radius);
 }
 
 BEGIN_MESSAGE_MAP(CGProjectDlg, CDialogEx)
-	ON_WM_SYSCOMMAND()
-	ON_WM_PAINT()
-	ON_WM_QUERYDRAGICON()
+    ON_WM_SYSCOMMAND()
+    ON_WM_PAINT()
+    ON_WM_QUERYDRAGICON()
+    ON_WM_LBUTTONDOWN()
+    ON_WM_MOUSEMOVE()
+    ON_WM_LBUTTONUP()
+    ON_BN_CLICKED(IDC_BTN_RESET, &CGProjectDlg::OnResetButtonClick)
+    ON_BN_CLICKED(IDC_BTN_RANDOM, &CGProjectDlg::OnRandomMoveButtonClick)
+    ON_BN_CLICKED(IDC_BTN_APPLY_RADIUS, &CGProjectDlg::OnApplyRadiusButtonClick)
 END_MESSAGE_MAP()
 
 
@@ -72,49 +82,57 @@ END_MESSAGE_MAP()
 
 BOOL CGProjectDlg::OnInitDialog()
 {
-	CDialogEx::OnInitDialog();
+    CDialogEx::OnInitDialog();
 
-	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+    // 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
 
-	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
-	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
-	ASSERT(IDM_ABOUTBOX < 0xF000);
+    // IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+    ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+    ASSERT(IDM_ABOUTBOX < 0xF000);
 
-	CMenu* pSysMenu = GetSystemMenu(FALSE);
-	if (pSysMenu != nullptr)
-	{
-		BOOL bNameValid;
-		CString strAboutMenu;
-		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
-		ASSERT(bNameValid);
-		if (!strAboutMenu.IsEmpty())
-		{
-			pSysMenu->AppendMenu(MF_SEPARATOR);
-			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
-		}
-	}
+    CMenu* pSysMenu = GetSystemMenu(FALSE);
+    if (pSysMenu != nullptr)
+    {
+        BOOL bNameValid;
+        CString strAboutMenu;
+        bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+        ASSERT(bNameValid);
+        if (!strAboutMenu.IsEmpty())
+        {
+            pSysMenu->AppendMenu(MF_SEPARATOR);
+            pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+        }
+    }
 
-	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
-	//  프레임워크가 이 작업을 자동으로 수행합니다.
-	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
-	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+    // 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+    //  프레임워크가 이 작업을 자동으로 수행합니다.
+    SetIcon(m_hIcon, TRUE);         // 큰 아이콘을 설정합니다.
+    SetIcon(m_hIcon, FALSE);      // 작은 아이콘을 설정합니다.
 
-	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+    // TODO: 여기에 추가 초기화 작업을 추가합니다.
+    UpdateData(FALSE);
+    m_radius = 10;
 
-	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+    m_staticCircle1.SetWindowTextW(L"Circle 1: (N/A, N/A)");
+    m_staticCircle2.SetWindowTextW(L"Circle 2: (N/A, N/A)");
+    m_staticCircle3.SetWindowTextW(L"Circle 3: (N/A, N/A)");
+
+    m_circleCount = 0;
+
+    return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
 }
 
 void CGProjectDlg::OnSysCommand(UINT nID, LPARAM lParam)
 {
-	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
-	{
-		CAboutDlg dlgAbout;
-		dlgAbout.DoModal();
-	}
-	else
-	{
-		CDialogEx::OnSysCommand(nID, lParam);
-	}
+    if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+    {
+        CAboutDlg dlgAbout;
+        dlgAbout.DoModal();
+    }
+    else
+    {
+        CDialogEx::OnSysCommand(nID, lParam);
+    }
 }
 
 // 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
@@ -123,33 +141,213 @@ void CGProjectDlg::OnSysCommand(UINT nID, LPARAM lParam)
 
 void CGProjectDlg::OnPaint()
 {
-	if (IsIconic())
-	{
-		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+    if (IsIconic())
+    {
+        CDialogEx::OnPaint();
+    }
+    else
+    {
+        CDialogEx::OnPaint();
 
-		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+        CClientDC dc(this);
 
-		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
-		int cxIcon = GetSystemMetrics(SM_CXICON);
-		int cyIcon = GetSystemMetrics(SM_CYICON);
-		CRect rect;
-		GetClientRect(&rect);
-		int x = (rect.Width() - cxIcon + 1) / 2;
-		int y = (rect.Height() - cyIcon + 1) / 2;
+        // 원 그리기
+        for (int i = 0; i < m_circleCount; i++)
+        {
+            CPoint point = m_circlePoints[i];
 
-		// 아이콘을 그립니다.
-		dc.DrawIcon(x, y, m_hIcon);
-	}
-	else
-	{
-		CDialogEx::OnPaint();
-	}
+            // 반지름을 m_radius로 변경하여 원 그리기
+            CPen pen(PS_SOLID, 2, RGB(0, 0, 0)); // 검은색 실선
+            CBrush brush(RGB(0, 0, 0)); // 검은색 채우기
+            CPen* oldPen = dc.SelectObject(&pen);
+            CBrush* oldBrush = dc.SelectObject(&brush);
+
+            // 반지름을 m_radius로 설정
+            dc.Ellipse(point.x - m_radius, point.y - m_radius, point.x + m_radius, point.y + m_radius);
+
+            dc.SelectObject(oldPen);
+            dc.SelectObject(oldBrush);
+        }
+
+        // 외접원 그리기 코드 (3개 원이 그려졌을 때만)
+        if (m_circleCount == 3)
+        {
+            // 세 점의 좌표 얻기
+            CPoint p1 = m_circlePoints[0];
+            CPoint p2 = m_circlePoints[1];
+            CPoint p3 = m_circlePoints[2];
+
+            // 외접원의 계산 및 그리기
+            double D = 2 * (p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y));
+            double Dx = (p1.x * p1.x + p1.y * p1.y) * (p2.y - p3.y) + (p2.x * p2.x + p2.y * p2.y) * (p3.y - p1.y) + (p3.x * p3.x + p3.y * p3.y) * (p1.y - p2.y);
+            double Dy = (p1.x * p1.x + p1.y * p1.y) * (p3.x - p2.x) + (p2.x * p2.x + p2.y * p2.y) * (p1.x - p3.x) + (p3.x * p3.x + p3.y * p3.y) * (p2.x - p1.x);
+            double cx = Dx / D;
+            double cy = Dy / D;
+
+            // 반지름 계산
+            double r = sqrt((p1.x - cx) * (p1.x - cx) + (p1.y - cy) * (p1.y - cy));
+
+            // 외접원 그리기
+            CPen pen(PS_SOLID, 2, RGB(0, 0, 0));
+            CPen* oldPen = dc.SelectObject(&pen);
+            dc.SelectStockObject(NULL_BRUSH);
+            dc.Ellipse((int)(cx - r), (int)(cy - r), (int)(cx + r), (int)(cy + r));
+            dc.SelectObject(oldPen);
+        }
+    }
+}
+
+void CGProjectDlg::OnResetButtonClick()
+{
+    // 원의 개수와 좌표를 초기화
+    m_circleCount = 0;
+    m_circlePoints.RemoveAll(); // CArray의 모든 항목 삭제
+
+    // m_radius 값을 초기값(10)으로 재설정
+    m_radius = 10;
+
+    // IDC_EDIT_RADIUS 컨트롤의 텍스트를 비움
+    GetDlgItem(IDC_EDIT_RADIUS)->SetWindowTextW(L"0");
+
+    UpdateCircleCoordinates();
+    Invalidate(); // 화면을 다시 그려서 원들을 제거
+}
+
+void CGProjectDlg::OnLButtonDown(UINT nFlags, CPoint point)
+{
+    CDialogEx::OnLButtonDown(nFlags, point);
+
+    m_selectedCircle = -1; // 초기화
+
+    // 기존 원을 클릭했는지 확인 (드래그 기능)
+    for (int i = 0; i < m_circleCount; i++)
+    {
+        CPoint p = m_circlePoints[i];
+        int dx = point.x - p.x;
+        int dy = point.y - p.y;
+        if (dx * dx + dy * dy <= m_radius * m_radius) // 반지름 m_radius 안에 있으면 선택
+        {
+            m_selectedCircle = i;
+            m_isDragging = true;
+            return; // 원이 이미 존재하면 추가하지 않고 종료
+        }
+    }
+
+    // 원이 3개 이상 그려지지 않도록
+    if (m_circleCount < 3)
+    {
+        // 원의 좌표를 저장하고 반지름을 m_radius로 설정
+        m_circlePoints.Add(point); // 원의 좌표 저장
+        m_circleCount++;
+        UpdateCircleCoordinates();
+        Invalidate(); // 화면 다시 그리기
+    }
+}
+
+void CGProjectDlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+    if (m_isDragging && m_selectedCircle != -1)
+    {
+        m_circlePoints[m_selectedCircle] = point; // 원 이동
+        UpdateCircleCoordinates();
+        Invalidate(); // 화면 갱신
+    }
+
+    CDialogEx::OnMouseMove(nFlags, point);
+}
+
+void CGProjectDlg::OnLButtonUp(UINT nFlags, CPoint point)
+{
+    m_isDragging = false; // 드래그 종료
+    m_selectedCircle = -1; // 선택 해제
+
+    CDialogEx::OnLButtonUp(nFlags, point);
+}
+
+
+void CGProjectDlg::UpdateCircleCoordinates()
+{
+    CString text;
+    if (m_circleCount > 0)
+    {
+        text.Format(L"Circle 1: (%d, %d)", m_circlePoints[0].x, m_circlePoints[0].y);
+        m_staticCircle1.SetWindowTextW(text);
+    }
+    else
+    {
+        m_staticCircle1.SetWindowTextW(L"Circle 1: (N/A, N/A)");
+    }
+
+    if (m_circleCount > 1)
+    {
+        text.Format(L"Circle 2: (%d, %d)", m_circlePoints[1].x, m_circlePoints[1].y);
+        m_staticCircle2.SetWindowTextW(text);
+    }
+    else
+    {
+        m_staticCircle2.SetWindowTextW(L"Circle 2: (N/A, N/A)");
+    }
+
+    if (m_circleCount > 2)
+    {
+        text.Format(L"Circle 3: (%d, %d)", m_circlePoints[2].x, m_circlePoints[2].y);
+        m_staticCircle3.SetWindowTextW(text);
+    }
+    else
+    {
+        m_staticCircle3.SetWindowTextW(L"Circle 3: (N/A, N/A)");
+    }
+}
+
+
+void CGProjectDlg::OnRandomMoveButtonClick()
+{
+    if (m_circleCount < 3) return;
+
+    if (m_isRunning.load()) return;
+
+    m_isRunning.store(true);
+    m_randomMoveThread = std::thread(&CGProjectDlg::RandomMoveCircles, this);
+    m_randomMoveThread.detach();
+}
+
+void CGProjectDlg::RandomMoveCircles()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    CRect clientRect;
+    GetClientRect(&clientRect);
+
+    std::uniform_int_distribution<int> distX(20, clientRect.Width() - 20);
+    std::uniform_int_distribution<int> distY(20, clientRect.Height() - 20);
+
+    for (int i = 0; i < 10; ++i)
+    {
+        if (!m_isRunning.load()) return;
+
+        for (int j = 0; j < 3; ++j)
+        {
+            m_circlePoints[j] = CPoint(distX(gen), distY(gen));
+        }
+
+        UpdateCircleCoordinates();
+        Invalidate();
+        Sleep(500);
+    }
+
+    m_isRunning.store(false);
+}
+
+void CGProjectDlg::OnApplyRadiusButtonClick()
+{
+    UpdateData(TRUE); // Edit Control의 값을 m_radius 변수에 업데이트
+    UpdateCircleCoordinates();
+    Invalidate();  // 화면을 다시 그려서 원을 갱신
 }
 
 // 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
 //  이 함수를 호출합니다.
 HCURSOR CGProjectDlg::OnQueryDragIcon()
 {
-	return static_cast<HCURSOR>(m_hIcon);
+    return static_cast<HCURSOR>(m_hIcon);
 }
-
--- GSolution/GProject/GProjectDlg.h
+++ GSolution/GProject/GProjectDlg.h
@@ -3,6 +3,10 @@
 //
 
 #pragma once
+#include "afxwin.h"
+#include <thread>
+#include <atomic>
+#include <random>
 
 
 // CGProjectDlg 대화 상자
@@ -24,11 +28,34 @@ public:
 // 구현입니다.
 protected:
 	HICON m_hIcon;
+	CArray<CPoint, CPoint> m_circlePoints;
+	int m_circleCount;
+	int m_selectedCircle;
+	bool m_isDragging;
+
+	CStatic m_staticCircle1;
+	CStatic m_staticCircle2;
+	CStatic m_staticCircle3;
 
 	// 생성된 메시지 맵 함수
 	virtual BOOL OnInitDialog();
 	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnResetButtonClick();
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnRandomMoveButtonClick();
+	afx_msg void OnApplyRadiusButtonClick();
+
+	void UpdateCircleCoordinates();
+	void RandomMoveCircles();
+
+	std::atomic<bool> m_isRunning;
+	std::thread m_randomMoveThread;
+
 	DECLARE_MESSAGE_MAP()
+public:
+	double m_radius;
 };
